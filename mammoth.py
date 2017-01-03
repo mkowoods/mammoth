c9f5cdee07ba2fbe249081bc41f2fe9bf684332d
M Woods <mkowoods@gmail.com>
2017-01-02 22:53:46 -0800

    more functional

--- mammoth.py
+++ mammoth.py
@@ -8,6 +8,8 @@ from flask import Flask, render_template, request
 from tinydb import TinyDB, Query
 import json
 import logging
+import time
+import base64
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -15,11 +17,16 @@ DB = TinyDB('./data/db.json')
 
 @app.route('/')
 def landing_page():
-    return'<h3>Landing Page </h3>'
+    all_tasks = DB.search(Query()['task_type'] != '')
+    return render_template('table.html', data = all_tasks)
 
-@app.route('/create_task/<api_key>')
-def create_task(api_key):
-    return render_template('create_task.html')
+@app.route('/create_phone_task/<api_key>')
+def create_phone_task(api_key):
+    return render_template('create_phone_task.html')
+
+@app.route('/create_file_review_task/<api_key>')
+def create_file_review_task(api_key):
+    return render_template('create_file_review.html')
 
 @app.route('/work_task/<task_id>')
 def work_task(task_id):
@@ -33,16 +40,36 @@ def work_task(task_id):
 def add_task(api_key): #TODO: need to modify to only take post commnands
     data = {
         'api_key': api_key,
-        'solved': False
+        'solved': False,
+        'create_date': time.time()
     }
-    form_data = request.form['data']
-    form_data = json.loads(form_data)
+    if request.files:
+        file = request.files['file']
+        data['file_name'] = file.filename
+        data['file_data'] = base64.b64encode( file.stream.read() )
+        for k, v in request.form.items():
+            data[k] = v
+    else:
+        form_data = request.form['data']
+        form_data = json.loads(form_data)
 
-    for k,v in form_data.items():
-        data[k] = v
+        for k,v in form_data.items():
+            data[k] = v
+
     id = DB.insert(data)
 
     logging.info('Logging Task ID: %d, data: %s'% (id, data))
     return str(id)
+
+@app.route('/mark_completed/<task_id>', methods=['POST'])
+def update_task(task_id):
+    data = request.form
+    DB.update({'solved': True,
+               'comment': data['comment'],
+               'option_choice': data['option_choice']},
+              eids=[int(task_id)])
+
+    return 'true'
+
 if __name__ == '__main__':
     app.run(debug = True, port=5001)
